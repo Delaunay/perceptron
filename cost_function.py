3746abc86353e2a737c3842821bc3e76d68cadc2
pierre delaunay <pierre.delaunay@outlook.com>
2015-01-10 12:23:41 -0500

    first

---
+++ cost_function.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Pierre Delaunay'
+
+
+# This file will implement the basic cost function / learning algorithm
+# features
+#
+# uses Theano and PyLearn2
+#
+
+# - recommended NN using convo
+# - the article speaks of SVM using kernels
